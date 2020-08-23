769fd8ca38adb27913c718bf5fcd2ce14f09090b
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-08-22 23:43:27 -0300

    Setup for use CRNA with monorepo

--- packages/mobile/App.js
+++ packages/mobile/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+      <Feather name="github" size={32} color="#000" />
     </View>
   );
 }
